08f5139660c672dbbc78db1e51a657ef0d5e26f2
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-02 12:15:08 -0500

    Write the associations between the User, Event and EventAttending models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+    has_many :attendees, through: :event_attendings
+    has_many :event_attendings
+    belongs_to :host, class_name: "User"
 end
--- app/models/event_attending.rb
+++ app/models/event_attending.rb
@@ -1,2 +1,5 @@
 class EventAttending < ApplicationRecord
+    belongs_to :event
+    belongs_to :attendee, class_name: "User"
+    belongs_to :creator, class_name: "User"
 end
