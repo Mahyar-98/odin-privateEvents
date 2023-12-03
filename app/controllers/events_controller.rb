f5d4ec8644eed7fe152c3e178d8aff07a94511d3
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-03 01:02:42 -0500

    write new and create event controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,23 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
     def index
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.events.build(allowed_params)
+        if @event.save
+            redirect_to @event
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def allowed_params
+        params.require(:event).permit(:date, :location)
     end
 end
